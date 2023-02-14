8e0601b10dbf4798240ed54910e672d835a8b8c4
Abraham Choi <abrahamchoi@icloud.com>
2023-02-13 18:32:27 -0800

    added error handling

--- 1_UserService/UserService.js
+++ 1_UserService/UserService.js
@@ -1,31 +1,64 @@
 class UserService {
-    constructor(id = 0) {
-        this.id = id;
+    constructor() {
         this.users = [];
+        this.id = 0;
     }
     addUser(user) {
-        this.id++;
-        user.id = this.id;
-        this.users.push(user);
+        try {
+            if (!user.name || !user.age) {
+                throw new Error("user name and age is required!");
+            }
+            this.id++;
+            user.id = this.id;
+            this.users.push(user);
+        }
+        catch (error) {
+            console.error(error.message);
+        }
     }
     getUsers() {
         return this.users;
     }
     getUserById(id) {
-        return this.users.find(user => user.id == id);
+        try {
+            const user = this.users.find(user => user.id == id);
+            console.log("user = ", user);
+            if (!user) {
+                throw new Error(`user id ${id} does not exist!`);
+            }
+            return user;
+        }
+        catch (error) {
+            console.error(error.message);
+            return undefined;
+        }
     }
     updateUser(user) {
-        const index = this.users.findIndex(u => u.id == user.id);
-        if (index != -1) {
+        try {
+            const index = this.users.findIndex(u => u.id == user.id);
+            if (index === -1) {
+                throw new Error(`user id ${user.id} does not exist!`);
+            }
             this.users[index] = user;
+        }
+        catch (error) {
+            console.error(error.message);
         }
     }
     deleteUser(id) {
-        this.users = this.users.filter(user => user.id != id);
+        try {
+            this.users = this.users.filter(user => user.id != id);
+            if (this.getUserById(id)) {
+                throw new Error(`user ${id} could not be deleted!`);
+            }
+        }
+        catch (error) {
+            console.error(error.message);
+        }
     }
 }
 const userService = new UserService();
-// add 2 users
+// add 3 users
 userService.addUser({ id: 0, name: "Abe", age: 35 });
 userService.addUser({ id: 0, name: "Buffy", age: 1 });
 userService.addUser({ id: 0, name: "Vampire", age: 99 });
@@ -33,11 +66,11 @@ userService.addUser({ id: 0, name: "Vampire", age: 99 });
 const users = userService.getUsers();
 console.log("\n1. Print users \n", users);
 // print user with id 1
-let user = userService.getUserById(1);
+const user = userService.getUserById(1);
 console.log("\n2. Print user with id 1 \n", user);
 // print updated user with id 1
-userService.updateUser({ id: 1, name: "Abraham", age: 36 });
-user = userService.getUserById(1);
+userService.updateUser({ id: 0, name: "Abraham", age: 36 });
+const updatedUser = userService.getUserById(1);
 console.log("\n3. print updated user with id 1 \n", user);
 // delete user id 3 then print
 userService.deleteUser(3);
--- 1_UserService/UserService.ts
+++ 1_UserService/UserService.ts
@@ -4,58 +4,95 @@ interface User {
     age: number;
 }
 
-class UserService<T extends User> {
-    private users: T[] = [];
+class UserService {
+    private users: User[] = [];
+    private id = 0;
 
-    constructor(private id = 0) {}
+    addUser(user: User): void {
+        try {
 
-    addUser(user: T): void {
-        this.id++;
-        user.id = this.id;
-        this.users.push(user);
+            if(!user.name || !user.age) {
+                throw new Error("user name and age is required!");
+            }
+
+            this.id++;
+            user.id = this.id;
+            this.users.push(user);
+
+        }catch(error) {
+            console.error(error.message);
+        }
     }
 
-    getUsers(): T[] {
+    getUsers(): User[] {
         return this.users;
     }
 
-    getUserById(id: number): T | undefined {
-        return this.users.find(user => user.id == id);
+    getUserById(id: number): User | undefined {
+        try {
+
+            const user = this.users.find(user => user.id == id);
+            console.log("user = ", user);
+            if(!user){
+                throw new Error(`user id ${id} does not exist!`);
+            }
+            return user;
+
+        } catch (error) {
+            console.error(error.message);
+            return undefined;
+        }
     }
 
-    updateUser(user: T): void {
-        const index = this.users.findIndex(u => u.id == user.id);
-        if(index != -1) {
+    updateUser(user: User): void {
+        
+        try {
+            const index = this.users.findIndex(u => u.id == user.id);
+            if(index === -1) {
+                throw new Error(`user id ${user.id} does not exist!`);
+            }
             this.users[index] = user;
+            
+        } catch (error) {
+            console.error(error.message);
         }
     }
 
     deleteUser(id: number): void {
-        this.users = this.users.filter(user => user.id != id);
+
+        try {
+            this.users = this.users.filter(user => user.id != id);
+            if(this.getUserById(id)) {
+                throw new Error(`user ${id} could not be deleted!`);
+            }
+
+        } catch (error) {
+            console.error(error.message);
+        }
     }
 }
 
 const userService = new UserService();
 
-// add 2 users
+// add 3 users
 userService.addUser({id: 0, name: "Abe", age: 35});
 userService.addUser({id: 0, name: "Buffy", age: 1});
 userService.addUser({id: 0, name: "Vampire", age: 99});
 
-// print all users
+// 1. print all users
 const users = userService.getUsers();
 console.log("\n1. Print users \n", users);
 
-// print user with id 1
-let user = userService.getUserById(1);
+// 2. print user with id 1
+const user = userService.getUserById(1);
 console.log("\n2. Print user with id 1 \n", user);
 
-// print updated user with id 1
-userService.updateUser({id: 1, name: "Abraham", age: 36});
-user = userService.getUserById(1);
+// 3. print updated user with id 1
+userService.updateUser({id: 0, name: "Abraham", age: 36});
+const updatedUser = userService.getUserById(1);
 console.log("\n3. print updated user with id 1 \n", user);
 
-// delete user id 3 then print
+// 4. delete user id 3 then print
 userService.deleteUser(3);
 const updateUsers = userService.getUsers();
 console.log("\n4. delete user 3 then print \n", updateUsers);
